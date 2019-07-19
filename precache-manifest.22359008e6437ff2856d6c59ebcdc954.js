ce1e0548bd891798609d526a9310832afd12cfcf
sudhaBommakanti <mailme.sudha.k@gmail.com>
2019-07-19 15:20:37 +0200

    Updates

---
+++ precache-manifest.22359008e6437ff2856d6c59ebcdc954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4b8f44a16419875f1b074025a2159b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "219230796a49503dee55",
+    "url": "/monsters-rolodex/static/css/main.970fa3a5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "219230796a49503dee55",
+    "url": "/monsters-rolodex/static/js/main.45f011a3.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
